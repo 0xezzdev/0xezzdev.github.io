c8e1eafe938b53f815d3bc3ce6e7398a0aee9fdf
Ezzeldeen Mohamed Abdelwahab <108143353+0xezzdev@users.noreply.github.com>
2026-03-25 22:53:43 +0200

    Set site URL in astro.config.mjs
    Added site URL to the Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://0xezzdev.github.io', // السطر ده هو اللي هيحل المشكلة
   vite: {
     plugins: [tailwindcss()],
   },
